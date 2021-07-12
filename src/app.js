27a882c810af40fd798f9f2065ee84718d8b2086
Kenneth Le-Tran <kenneth.letran@gmail.com>
2021-07-12 16:45:56 -0500

    enable users to close modal via esc key or clicking outside of it, created background overlay while modal is open, change breakpoint for modal content from small to medium

--- src/app.js
+++ src/app.js
@@ -1,42 +1,66 @@
-// TODO: refactor into modules if too much
+// TODO: refactor into modules if code gets cluttered
+// Get DOM elements
+const menu = document.querySelector('.menu__button');
+const menuModalInner = document.querySelector('.menu__modal--inner');
+const menuModalOuter = document.querySelector('.menu__modal--outer');
+const heroBtn = document.querySelector('.hero__button');
+const body = document.body;
 
-// Menu
-const menuBtn = document.querySelector('.menu__button');
-const menuModal = document.querySelector('.menu__modal');
+// Store scroll position when menu is clicked
+function keepScrollPosition() {
+  body.classList.add('prevent-scroll');
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollY}`;
+}
 
+// Return to scroll position when modal is closed
+function returnToScrollPosition() {
+  body.classList.remove('prevent-scroll');
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+}
 
 function handleMenuClick() {
-  const body = document.body;
-
-  if(menuBtn.classList.contains('menu--open')) {
-    menuBtn.classList.remove('menu--open');
-    menuModal.classList.remove('modal--open');
-
-    const returnToScrollPosition = () => {
-      body.classList.remove('prevent-scroll');
-      const scrollY = body.style.top;
-      body.style.position = '';
-      body.style.top = '';
-      window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    }
+  if(menu.classList.contains('menu--open')) {
+    menu.classList.remove('menu--open');
+    menuModalInner.classList.remove('modal--open');
+    menuModalOuter.classList.remove('modal--open');
     returnToScrollPosition();
-    
   } else {
-    menuBtn.classList.add('menu--open');
-    menuModal.classList.add('modal--open');
-    
-    const keepScrollPosition = () => {
-      body.classList.add('prevent-scroll');
-      const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-      body.style.position = 'fixed';
-      body.style.top = `-${scrollY}`;
-    };
+    menu.classList.add('menu--open');
+    menuModalInner.classList.add('modal--open');
+    menuModalOuter.classList.add('modal--open');
     keepScrollPosition();
   }
 }
 
-menuBtn.addEventListener('click', handleMenuClick);
+// Open/close modal when menu is clicked
+menu.addEventListener('click', handleMenuClick);
+heroBtn.addEventListener('click', handleMenuClick);
 
+// Allow users to close modal by clicking outside of modal or by pressing ESC key
+menuModalOuter.addEventListener('click', function (e) {
+  const clickOutside = !e.target.closest('.menu__modal--inner');
+  if (clickOutside) {
+    menu.classList.remove('menu--open');
+    menuModalOuter.classList.remove('modal--open');
+    menuModalInner.classList.remove('modal--open');
+    returnToScrollPosition();
+  }
+});
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    menu.classList.remove('menu--open');
+    menuModalOuter.classList.remove('modal--open');
+    menuModalInner.classList.remove('modal--open');
+    returnToScrollPosition();
+  }
+});
+
+// Keep track of window scroll position
 window.addEventListener('scroll', () => {
   document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
 });
