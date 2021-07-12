b25d22915de87af70fe5b3c544211ad022e3265d
Kenneth Le-Tran <kenneth.letran@gmail.com>
2021-07-12 15:17:19 -0500

    turning off smooth scroll behavior, efixed issue where interacting with modal would lose current scroll position

--- src/app.js
+++ src/app.js
@@ -1,18 +1,42 @@
+// TODO: refactor into modules if too much
+
 // Menu
 const menuBtn = document.querySelector('.menu__button');
 const menuModal = document.querySelector('.menu__modal');
 
 
 function handleMenuClick() {
+  const body = document.body;
+
   if(menuBtn.classList.contains('menu--open')) {
     menuBtn.classList.remove('menu--open');
     menuModal.classList.remove('modal--open');
-    document.body.classList.remove('prevent-scroll');
+
+    const returnToScrollPosition = () => {
+      body.classList.remove('prevent-scroll');
+      const scrollY = body.style.top;
+      body.style.position = '';
+      body.style.top = '';
+      window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    }
+    returnToScrollPosition();
+    
   } else {
     menuBtn.classList.add('menu--open');
     menuModal.classList.add('modal--open');
-    document.body.classList.add('prevent-scroll');
+    
+    const keepScrollPosition = () => {
+      body.classList.add('prevent-scroll');
+      const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+      body.style.position = 'fixed';
+      body.style.top = `-${scrollY}`;
+    };
+    keepScrollPosition();
   }
 }
 
 menuBtn.addEventListener('click', handleMenuClick);
+
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+});
