9fb1f4106d3808135adbfe029db2859319f508e1
Kenneth Le-Tran <kenneth.letran@gmail.com>
2021-08-01 00:14:57 -0500

    adjusting box-shadow of project containers for specific mobile device sizes

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -484,7 +484,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41357" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39567" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
