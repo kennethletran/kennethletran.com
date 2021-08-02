b641425c7ba8e937b9b4724796baccd702caa3e1
Kenneth Le-Tran <kenneth.letran@gmail.com>
2021-08-02 18:33:47 -0500

    changing copy write on introduction

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -490,7 +490,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46263" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46275" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
